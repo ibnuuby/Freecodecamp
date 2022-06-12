b321d3cea3ab995e8bed8c48dbd7d17d0367d195
ibnuubaeydillah13 <ibnuubaeydillah13@gmail.com>
2022-06-12 15:02:02 +0700

    add day1

---
+++ Learn Python/Day-1/introduction.py
@@ -0,0 +1,100 @@
+# Introduction to Python
+# 1.Topics
+#   -Numbers
+#   -Strings
+#   -Booleans
+#   -Dictionaries
+#   -Variable
+#   -Loops
+#   -Iterators
+#   and much more!
+
+# Operator Aritmatic
+
+# Operator  Name            Example
+#  +        Addition        2+2
+#  -        Subtraction     3-1
+#  *        Multiplication  5*3
+#  /        Division        5/2
+#  %        Modulus         5%2
+#  //       Floor division  9//2
+#  **       Exponential     2**4
+
+#Operator Predence
+
+# >>> 2 + 3 * 3
+# 11
+# >>> (2 + 3) * 3
+# 15
+# >>> 1 + 2 ** 2
+# 5
+# >>> 2 / 2 * 8
+# 8.0
+
+#Using the underscore to get previous result
+
+# >>> 3 * 3
+# 9
+# >>> _ + 3
+# 12
+
+#Variables
+# a = 5
+# result = 3*5
+# print(result)
+
+#Strings
+# 'Belajar Python'
+
+# >>> 'a' + 'b'
+# 'ab'
+# >>> 'ab' * 4
+# 'abababab'
+# >>> 'a' - 'b'
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: unsupported operand type(s) for -: 'str' and 'str'
+
+#Escaping
+
+#Fungsi dari escaping untuk menghidanari dari error penggunakan strings khusunya menggunakan tanda ' sebagai tambahan pada kalimat
+
+#>>> mystring = 'It\'s an escaped quote!'
+# >>> mystring
+# "It's an escaped quote!"
+
+#Multi Strings
+# >>> """This is line 1,
+# ... this is line 2,
+# ... this is line 3."""
+# 'This is line 1,\nthis is line 2,\nthis is line 3.'
+
+# Using triple quotes for escaping
+
+# >>> line = """He said: "Hello, I've got a question" from the audience"""
+
+#String operations
+# >>> mystring = "Hello world"
+# >>> mystring.
+# mystring.capitalize(    mystring.find(          mystring.isdecimal(     mystring.istitle(       mystring.partition(     mystring.rstrip(        mystring.translate(
+# mystring.casefold(      mystring.format(        mystring.isdigit(       mystring.isupper(       mystring.replace(       mystring.split(         mystring.upper(
+# mystring.center(        mystring.format_map(    mystring.isidentifier(  mystring.join(          mystring.rfind(         mystring.splitlines(    mystring.zfill(
+# mystring.count(         mystring.index(         mystring.islower(       mystring.ljust(         mystring.rindex(        mystring.startswith(
+# mystring.encode(        mystring.isalnum(       mystring.isnumeric(     mystring.lower(         mystring.rjust(         mystring.strip(
+# mystring.endswith(      mystring.isalpha(       mystring.isprintable(   mystring.lstrip(        mystring.rpartition(    mystring.swapcase(
+# mystring.expandtabs(    mystring.isascii(       mystring.isspace(       mystring.maketrans(     mystring.rsplit(        mystring.title(
+
+#Getting the string length
+
+#Function Len() digunakan untuk menghitung jumlah panjang string yang ada dalam varible
+
+#Example
+#   len(mystring)
+#   >>11
+
+#String a string
+#   'Hello World'.split('')
+#   ['Hello', 'World']
+
+
+print("Im are learn python laguange")
