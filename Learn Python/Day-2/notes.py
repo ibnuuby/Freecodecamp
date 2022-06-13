57467d2dbafe937d8bbfc6d866ef7b6b654f5419
ibnuubaeydillah13 <ibnuubaeydillah13@gmail.com>
2022-06-13 11:54:06 +0700

    learn python day 2

---
+++ Learn Python/Day-2/notes.py
@@ -0,0 +1,93 @@
+# Python string formatting with f-strings
+# fungsi f-string digunakan untuk menggabungkan antara int dan string
+
+# Example :
+
+# >>> my_age = 40  // this is int
+# >>> f'My age is {my_age}' // use function f-strings, to change int to string.
+# My age is 40
+
+# >>> f'3 + 4 = {3+4}'
+# '3 + 4 = 7'
+# >>> my_age = 40
+# >>> f'My age is, unfortunately, not {my_age-8}'
+# 'My age is, unfortunately, not 32'
+
+# name = "John"
+# age = 56
+
+# print(f"{name=}, {age=}")
+# >>>name='John', age=56
+
+#Function
+
+#   >Print Function
+# print(1)
+# print('Some text')
+# print('A tab:\t and some text')
+# print()
+# print('This is my age:', 39)
+
+# door = True
+# if door:
+#     print("hi, open the door!")
+
+# door = False
+# if door:
+#     print("hi, open the door!")
+# else:
+#     print("hi, close the door for you!")
+
+#Python Operators
+
+# > Comparison Operator
+
+# Example 
+# >>> 2 > 1
+# True
+# >>> 2 < 1
+# False
+# >>> 2 < 3 < 4 < 5 < 6
+# True
+# >>> 2 < 3 > 2
+# True
+# >>> 3 <= 3
+# True
+# >>> 3 >= 2
+# True
+# >>> 2 == 2
+# True
+# >>> 4 != 5
+# True
+# >>> 'a' == 'a'
+# True
+# >>> 'a' > 'b'
+# False
+
+#Logical Operator
+
+# Operator                   What is does                   Examples
+#   and          True if both statements are true   
+
+# Comparing different types in Python
+
+# >>> True == 1
+# True
+# >>> False == 0
+# True
+# >>> True + True
+# 2
+# >>> False + False
+# 0
+# >>> False + True
+# 1
+# >>> True + 3
+# 4
+
+# Python For-loop
+
+
+for book in ('hello'):
+    print(book)
+
+
